cd2506b470c6caa2816ece142fabc5d6c06dde62
winsssHub <winstonmangori001@gmail.com>
2025-06-14 22:48:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 import numpy as np
 import joblib
 
 app = Flask(__name__)
 
-# Load model dan label encoder
+API_KEY = '0441aae10855a19beaa714221628d566'
 model = joblib.load('model_cuaca.pkl')
 le = joblib.load('label_encoder.pkl')
 
@@ -14,18 +15,42 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    city = request.form['city']
+
     try:
-        precipitation = float(request.form['precipitation'])
-        temp_max = float(request.form['temp_max'])
-        wind = float(request.form['wind'])
+        url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        data = response.json()
 
-        features = np.array([[precipitation, temp_max, wind]])
-        prediction = model.predict(features)
-        weather_pred = le.inverse_transform(prediction)[0]
+        predictions = []
+        used_days = set()
+        
+        for entry in data['list']:
+            dt_txt = entry['dt_txt'].split()[0]
+            if dt_txt in used_days:
+                continue  # hanya ambil 1x per hari
+            used_days.add(dt_txt)
 
-        return jsonify({'prediction': f"Perkiraan cuaca: {weather_pred}"})
+            precipitation = entry.get('rain', {}).get('3h', 0.0)
+            temp_max = entry['main']['temp_max']
+            wind = entry['wind']['speed']
+
+            features = np.array([[precipitation, temp_max, wind]])
+            pred_label = model.predict(features)
+            weather = le.inverse_transform(pred_label)[0]
+
+            predictions.append({
+                'date': dt_txt,
+                'weather': weather
+            })
+
+            if len(predictions) == 5:
+                break
+
+        return jsonify({'forecast': predictions})
+
     except Exception as e:
-        return jsonify({'prediction': f"Terjadi kesalahan: {str(e)}"})
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
